3ed1f782d85eac359b4d09e1f4fd05dba472723b
Víctor Gómez <gomezatabales.v@gmail.com>
2023-11-22 00:04:35 -0300

    Proyecto corriendo con handlebars, express y tailwindcss.

--- app.js
+++ app.js
@@ -4,12 +4,13 @@ const hbs=require('hbs');
 const app= express()
 
 app.set('view engine','hbs');
-app.use(express.static('dist'))
+app.use(express.static('public'))
+const port = 3000
 
 hbs.registerPartials(__dirname+'/views/partials',function(err){});
 
 app.get('/',(req,res)=>{//esto es un controlador
-    res.render('home',{
+    res.render('index',{
         titulo:'Titulo dínamico',
         nombre:'titulo'
     })
@@ -33,5 +34,6 @@ app.get('/elements',(req,res)=>{
 
 })*/
 
-
-app.listen(8081)
+app.listen(port, () => {
+    console.log(`Listening on http://localhost:${port}/ `)
+  })
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme');
 module.exports = {
-  content:  ["./src/**/*.{html,js}"],
+  content: ['./public/**/*.{html,js}','./views/**/*.hbs'],
+  darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        primary:'#0f172a'
+      },
+      fontFamily:{
+        roboto:['Roboto',...defaultTheme.fontFamily.sans]//si el usuario no tiene roboto se aplica defaul sans
+      }
+    },
   },
   plugins: [],
 }
